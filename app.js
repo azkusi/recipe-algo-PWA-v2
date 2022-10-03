dc74c009c20738055648223dd3a962889e47d155
azkusi <azkusi1@gmail.com>
2022-10-03 19:00:58 +0100

    change of build path

--- app.js
+++ app.js
@@ -12,12 +12,12 @@ app.listen(port, () => {
 app.use(cors());
 
 
-app.use(express.static(path.resolve(__dirname, 'client/build')));
+app.use(express.static(path.resolve(__dirname, 'build')));
 
 
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
 
 
